697e7cbc23c68a6689c4de3c69e094f5f1cbe6f1
khoi.le <thekhoi.k@gmail.com>
2025-05-30 15:56:50 +0700

    update next ver

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -25,6 +25,7 @@ export default function LocaleLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link href="/images/favicon.png" rel="shortcut icon" type="image/x-icon" />
       </head>
       <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,16 +10,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+  return children;
 } 
