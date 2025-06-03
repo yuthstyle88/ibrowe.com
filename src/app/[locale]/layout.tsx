8a8298126df2f45e3a49ca998f63bb0212b767a3
koey <thekhoi.k@gmail.com>
2025-06-03 23:39:38 +0700

    fix await error locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,7 +12,7 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const { locale } = params;
+  const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
