f47c9730fd1daf1b872aeb7e5277a0697d2f54df
khoi.le <thekhoi.k@gmail.com>
2025-06-04 11:22:07 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const createNextIntlPlugin = require('next-intl/plugin');
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  output: 'standalone',
+};
+
 module.exports = withNextIntl(nextConfig);
