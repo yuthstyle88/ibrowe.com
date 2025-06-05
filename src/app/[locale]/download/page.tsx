65f2d8602d4fa73aec3f08f4f8e92e278d6cb6c7
koey <thekhoi.k@gmail.com>
2025-06-05 23:24:19 +0700

    update thai lang download page

--- src/app/[locale]/download/page.tsx
+++ src/app/[locale]/download/page.tsx
@@ -9,8 +9,7 @@ export default function Download() {
 
   return (
     <main>
-      <Navigation />
-      <div className="bg-white">
+      <div className="bg-white py-28">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
           <div className="text-center">
             <h1 className="text-4xl font-bold text-gray-900 mb-8">
