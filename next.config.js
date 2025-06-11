acae2f512b5f35e7f26e5a6420a0945f5902d0b9
khoi.le <thekhoi.k@gmail.com>
2025-06-11 16:18:09 +0700

    remove redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'support.ibrowe.com',
+            },
+          ],
+          destination: '/support/en/:path*',
+        },
+      ],
+    };
+  },
+};
  
 module.exports = withNextIntl(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import { redirect } from 'next/navigation';
-import { headers } from 'next/headers';
 
-export default async function RootPage() {
-  const headersList = await headers();
-  const host = headersList.get('host') || '';
-  const subdomain = host.split('.')[0];
-
-  if (subdomain === 'support') {
-    redirect('/support/en');
-  }
-
+export default function RootPage() {
   redirect('/en');
 } 
