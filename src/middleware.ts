8a66e166ba795504ff0c12064dab970d789301de
khoi.le <thekhoi.k@gmail.com>
2025-06-25 11:23:23 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default createMiddleware({
   locales: routing.locales,
   defaultLocale: routing.defaultLocale,
-  localePrefix: 'always',
-  localeDetection: true
+  localeDetection: true,
+  localePrefix: isProd ? 'never' : 'always'
 });
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)']
-}; 
+};
